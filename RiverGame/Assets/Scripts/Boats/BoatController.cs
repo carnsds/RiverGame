25e207d6838339134ac9e0167c00fe81bb44bf71
Joshua Shook <shookjoshua1986@gmail.com>
2018-10-20 16:15:27 -0400

    Examples for when creating an object with Unity, having objects talk to each other, and grabbing multiple objects with the same tag.

---
+++ RiverGame/Assets/Scripts/Boats/BoatController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatController : MonoBehaviour
+{
+	private int value;
+	public int store;
+
+	private EnemyController[] enemies;
+
+	//private GameObject enemy;
+	private EnemyController enemy;
+
+	// Use this for initialization
+	void Start()
+	{
+		GameObject[] obj = GameObject.FindGameObjectsWithTag ("Enemy");
+		enemies = new EnemyController[obj.Length];
+		for (int i = 0; i < obj.Length; i++)
+		{
+			enemies[i] = obj[i].GetComponent<EnemyController>();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKeyDown (KeyCode.F)) {
+			foreach (EnemyController i in enemies)
+			{
+				i.health -= 10;
+			}
+		}
+	}
+}
