9043eb90a2a007e9301add58a1fe51681b904983
Matthew Glancy <matthewglancy1@gmail.com>
2018-10-24 22:23:52 -0400

    The select feature has been implemented and is functioning on my machine. You are able to click on a boat to select it, then click on another boat to deselect the last one and select the new. This will be used later to determine which boat the user wants to make changes with, move, etc.

--- RiverGame/Assets/Scripts/Player/BoatController.cs
+++ RiverGame/Assets/Scripts/Player/BoatController.cs
@@ -2,45 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-/**
- * Anything marked with the comment "RL", or surrounded
- * with that comment, indicates it needs to be removed
- * later once we're done using the examples.
- **/
 public class BoatController : MonoBehaviour
 {
 	public int boatHealth;
 
-	private EnemyController[] enemies; //RL
-	private EnemyController enemy; //RL
+	private int id;
 
 	public void Start()
 	{
-		//RL TOP
-		GameObject[] obj = GameObject.FindGameObjectsWithTag ("Enemy");
-		enemies = new EnemyController[obj.Length];
-		for (int i = 0; i < obj.Length; i++)
-		{
-			enemies[i] = obj[i].GetComponent<EnemyController>();
-		}
-		//RL BOTTOM
+		
 	}
 	
 	public void Update()
 	{
-		if (boatHealth >= 0)
+		if (boatHealth <= 0)
 		{
 			Destroy(gameObject);
 		}
+	}
 
-		//RL TOP
-		if (Input.GetKeyDown(KeyCode.F))
-		{
-			foreach (EnemyController i in enemies)
-			{
-				i.health -= 10;
-			}
-		}
-		//RL BOTTOM
+	/**
+	 * When a boat is clicked on, mark it as selected.
+	 **/
+	public void OnMouseDown()
+	{
+		tag = "Selected";
+		GetComponentInParent<BoatManager>().setCurrentSelected(id);
+	}
+
+	public void setID(int id)
+	{
+		this.id = id;
+	}
+
+	public int getID()
+	{
+		return id;
 	}
 }
--- RiverGame/Assets/Scripts/Player/BoatManager.cs
+++ RiverGame/Assets/Scripts/Player/BoatManager.cs
@@ -4,20 +4,80 @@ using UnityEngine;
 
 public class BoatManager : MonoBehaviour
 {
-	private List<GameObject> boats;
+	public List<GameObject> boats;
+
 	private List<BoatController> boatControllers;
+	private int currentSelected;
+	private int lastSelected;
 
 	public void Start()
 	{
+		currentSelected = -1;
+		lastSelected = -1;
+
+		boatControllers = new List<BoatController>();
+
 		/**
-		 * Fetches all the boats loaded into the scene and
-		 * adds them to our BoatManager.
-		 **/ 
-		GameObject[] boats = GameObject.FindGameObjectsWithTag("Boat");
+		 * Loads our boats into the scene.
+		 **/
+		for (int i = 0; i < boats.Capacity; i++)
+		{
+			Vector3 rot = new Vector3(0, 0, 90);
+
+			GameObject inst = Instantiate(boats[i], transform.position, Quaternion.Euler(rot), transform);
+			inst.name = "Boat" + i;
+			inst.transform.position = new Vector3 (Random.Range(-10, 10), 1, Random.Range(-10, 10));
+
+			//This replaces the prefab in the List with the actual object.
+			boats[i] = inst;
+
+			BoatController controller = (BoatController) inst.GetComponent<BoatController>();
+			controller.setID(i); //Assigns IDs based on order of instantiation.
+			boatControllers.Add(controller);
+		}
+	}
+
+	/**
+	 * This marks the ID of the currently selected boat. If there is
+	 * a previous it's saved to compare so we only deselect the boat
+	 * if a new one is being selected.
+	 * 
+	 * @param id The ID of the boat we're selecting
+	 **/
+	public void setCurrentSelected(int id)
+	{
+		if (lastSelected != currentSelected)
+		{
+			findBoatWithID(currentSelected).tag = "Unselected";
+			lastSelected = currentSelected;
+			currentSelected = id;
+		}
+		else
+		{
+			currentSelected = id;
+		}
+	}
+
+	public int getCurrentSelected()
+	{
+		return currentSelected;
+	}
+
+	/**
+	 * Looks through the list and returns the boat with the specified ID.
+	 * 
+	 * @param id The ID of the boat we're searching for.
+	 * @return The boat with the given ID (null if there are none).
+	 **/
+	public GameObject findBoatWithID(int id)
+	{
 		foreach (GameObject boat in boats)
 		{
-			this.boats.Add(boat);
-			boatControllers.Add(boat.GetComponent<BoatController>());
+			if (boat.GetComponent<BoatController>().getID() == id)
+			{
+				return boat;
+			}
 		}
+		return null;
 	}
 }
