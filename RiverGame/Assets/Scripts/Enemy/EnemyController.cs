953221aeefce04e54858494de2bacc416dc4fcc7
Matthew Glancy <matthewglancy1@gmail.com>
2018-10-27 15:21:14 -0400

    The enemy is assigned with a RaycastShooter that will shoot a projectile whenever an object moves in front of it.

--- RiverGame/Assets/Scripts/Enemy/EnemyController.cs
+++ RiverGame/Assets/Scripts/Enemy/EnemyController.cs
@@ -5,14 +5,6 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
 	[SerializeField] private int health;
-	[SerializeField] private GameObject projectile;
-
-	private float time;
-
-	public void Start()
-	{
-		time = Time.time;
-	}
 
 	public void Update()
 	{
@@ -20,18 +12,5 @@ public class EnemyController : MonoBehaviour
 		{
 			Destroy(gameObject);
 		}
-
-		if (time + 3f < Time.time)
-		{
-			time = Time.time;
-			Shoot();
-		}
-	}
-
-	public void Shoot()
-	{
-		Vector3 pos = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z + 2f);
-		GameObject proj = Instantiate(projectile, pos, Quaternion.identity, transform.parent);
-		proj.GetComponent<Rigidbody>().velocity = new Vector3(10, 0, 0);
 	}
 }
---
+++ RiverGame/Assets/Scripts/Shooting/RayShooter.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayShooter : MonoBehaviour
+{
+	[SerializeField] private GameObject projectile;
+
+	private Ray ray;
+	private float time;
+
+	public void Start()
+	{
+		Vector3 pos = new Vector3 (transform.position.x + 10, transform.position.y, transform.position.z);
+		ray = new Ray(transform.position, pos);
+		time = Time.time;
+	}
+
+	public void Update()
+	{
+		RaycastHit hit;
+		if(Physics.Raycast(transform.position, transform.right, out hit, 10))
+		{
+			if (Time.time >= time + 2f)
+			{
+				time = Time.time;
+				Shoot ();
+			}
+		}
+	}
+
+	public void Shoot()
+	{
+		Vector3 pos = new Vector3(ray.origin.x + 2f, ray.origin.y + 1.5f, ray.origin.z);
+		GameObject proj = Instantiate(projectile, pos, Quaternion.identity, transform.parent);
+		proj.GetComponent<Rigidbody>().velocity = new Vector3(10, 0, 0);
+	}
+}
